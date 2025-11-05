ec95eb39c14fdc574648507d9590382f03aa8631
mingwan21 <ibini12@gmail.com>
2025-11-06 00:52:44 +0900

    first commit

---
+++ src/main/java/com/example/adventcalendar/AdventcalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.adventcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventcalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventcalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adventcalendar/AdventcalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventcalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
