4932776bf433efcc4618c9e4bf0c9ef7662d41c9
mingwan21 <ibini12@gmail.com>
2025-11-11 23:17:41 +0900

    refactor: message 에서 letter 로 변경

---
+++ src/main/java/com/example/adventcalendar/repository/LetterRepository.java
@@ -0,0 +1,16 @@
+package com.example.adventcalendar.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.adventcalendar.entity.Letter;
+
+@Repository
+public interface LetterRepository extends JpaRepository<Letter, Long> {
+
+	List<Letter> findByUserId(Long userId);
+
+	List<Letter> findByUserIdAndDayLessThanEqual(Long userId, Integer day);
+}
